5eae82a43ba2f5e23eae80ea9e4dee1ec23ba8e1
Feng Zhou <zhoufwind@hotmail.com>
2019-07-25 10:50:54 +0800

    update greetings function comment

--- ex9.3-greetings/greetings/greetings.go
+++ ex9.3-greetings/greetings/greetings.go
@@ -1,31 +1,36 @@
 package greetings
 
 import "time"
-//import "fmt"
 
+// Print good day
 func GoodDay(name string) string {
     return "Good Day " + name
 }
 
+// Print good night
 func GoodNight(name string) string {
     return "Good Night " + name
 }
 
+// Print current time, with RFC822 time format
 func GetTime() string {
     localTime := time.Now()
     return localTime.Format(time.RFC822)
 }
 
+// If morning, return true
 func IsAM() bool {
     localTime := time.Now()
     return localTime.Hour() <= 12
 }
 
+// If afternoon, return true
 func IsAfternoon() bool {
     localTime := time.Now()
     return localTime.Hour() <= 18
 }
 
+// If evening, return true
 func IsEvening() bool {
     localTime := time.Now()
     return localTime.Hour() <= 22
