bc8e9fbf932222794394fc848f30b7a80d317b09
Feng Zhou <zhoufwind@hotmail.com>
2019-07-25 11:31:53 +0800

    add uc main func

---
+++ ex9.6-uc/ucmain.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "./uc"
+)
+
+// uc main function
+func main() {
+    str1 := "Using package uc!"
+    fmt.Println(uc.UpperCase(str1))
+}
+
+/*
+USING PACKAGE UC!
+*/
